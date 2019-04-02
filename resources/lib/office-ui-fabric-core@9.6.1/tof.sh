b6a7a85a47fa64d74fa77884fd41b07fe788a942
Marco Pinto <pinto@host-37-191-210-54.lynet.no>
2019-04-02 21:56:44 +0200

    Fix graphics

---
+++ resources/lib/office-ui-fabric-core@9.6.1/tof.sh
@@ -0,0 +1,97 @@
+wget -c https://static2.sharepointonline.com/files/fabric/assets/icons/fabricmdl2icons-2.68.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/icons/fabricmdl2icons-2.68.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/icons/fabricmdl2icons-2.68.ttf
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/selawik/selawik-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semilight.woff2 -O ./segoeui-westeuropean-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semilight.woff -O ./segoeui-westeuropean-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semibold.woff2 -O ./segoeui-westeuropean-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semibold.woff -O ./segoeui-westeuropean-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-regular.woff2 -O ./segoeui-westeuropean-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-regular.woff -O ./segoeui-westeuropean-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-light.woff2 -O ./segoeui-westeuropean-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-light.woff -O ./segoeui-westeuropean-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-bold.woff2 -O ./segoeui-westeuropean-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-bold.woff -O ./segoeui-westeuropean-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-semilight.woff2 -O ./segoeui-vietnamese-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-semilight.woff -O ./segoeui-vietnamese-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-semibold.woff2 -O ./segoeui-vietnamese-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-semibold.woff -O ./segoeui-vietnamese-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-regular.woff2 -O ./segoeui-vietnamese-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-regular.woff -O ./segoeui-vietnamese-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-light.woff2 -O ./segoeui-vietnamese-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-light.woff -O ./segoeui-vietnamese-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-bold.woff2 -O ./segoeui-vietnamese-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-vietnamese/segoeui-bold.woff -O ./segoeui-vietnamese-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-semilight.woff2 -O ./segoeui-hebrew-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-semilight.woff -O ./segoeui-hebrew-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-semibold.woff2 -O ./segoeui-hebrew-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-semibold.woff -O ./segoeui-hebrew-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-regular.woff2 -O ./segoeui-hebrew-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-regular.woff -O ./segoeui-hebrew-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-light.woff2 -O ./segoeui-hebrew-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-light.woff -O ./segoeui-hebrew-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-bold.woff2 -O ./segoeui-hebrew-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-hebrew/segoeui-bold.woff -O ./segoeui-hebrew-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-semilight.woff2 -O ./segoeui-greek-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-semilight.woff -O ./segoeui-greek-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-semibold.woff2 -O ./segoeui-greek-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-semibold.woff -O ./segoeui-greek-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-regular.woff2 -O ./segoeui-greek-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-regular.woff -O ./segoeui-greek-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-light.woff2 -O ./segoeui-greek-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-light.woff -O ./segoeui-greek-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-bold.woff2 -O ./segoeui-greek-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-greek/segoeui-bold.woff -O ./segoeui-greek-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-semilight.woff2 -O ./segoeui-easteuropean-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-semilight.woff -O ./segoeui-easteuropean-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-semibold.woff2 -O ./segoeui-easteuropean-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-semibold.woff -O ./segoeui-easteuropean-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-regular.woff2 -O ./segoeui-easteuropean-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-regular.woff -O ./segoeui-easteuropean-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-light.woff2 -O ./segoeui-easteuropean-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-light.woff -O ./segoeui-easteuropean-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-bold.woff2 -O ./segoeui-easteuropean-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-easteuropean/segoeui-bold.woff -O ./segoeui-easteuropean-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-semilight.woff2 -O ./segoeui-cyrillic-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-semilight.woff -O ./segoeui-cyrillic-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-semibold.woff2 -O ./segoeui-cyrillic-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-semibold.woff -O ./segoeui-cyrillic-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-regular.woff2 -O ./segoeui-cyrillic-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-regular.woff -O ./segoeui-cyrillic-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-light.woff2 -O ./segoeui-cyrillic-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-light.woff -O ./segoeui-cyrillic-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-bold.woff2 -O ./segoeui-cyrillic-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-cyrillic/segoeui-bold.woff -O ./segoeui-cyrillic-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-semilight.woff2 -O ./segoeui-arabic-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-semilight.woff -O ./segoeui-arabic-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-semibold.woff2 -O ./segoeui-arabic-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-semibold.woff -O ./segoeui-arabic-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-regular.woff2 -O ./segoeui-arabic-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-regular.woff -O ./segoeui-arabic-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-light.woff2 -O ./segoeui-arabic-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-light.woff -O ./segoeui-arabic-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-bold.woff2 -O ./segoeui-arabic-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-bold.woff -O ./segoeui-arabic-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semilight.woff2 -O ./leelawadeeui-thai-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semilight.woff2 -O ./leelawadeeui-thai-semilight.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semilight.woff -O ./leelawadeeui-thai-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semilight.woff -O ./leelawadeeui-thai-semilight.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semibold.woff2 -O ./leelawadeeui-thai-semibold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-semibold.woff -O ./leelawadeeui-thai-semibold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-regular.woff2 -O ./leelawadeeui-thai-regular.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-regular.woff -O ./leelawadeeui-thai-regular.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-light.woff2 -O ./leelawadeeui-thai-light.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-light.woff -O ./leelawadeeui-thai-light.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-bold.woff2 -O ./leelawadeeui-thai-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-bold.woff2 -O ./leelawadeeui-thai-bold.woff2
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-bold.woff -O ./leelawadeeui-thai-bold.woff
+wget -c https://static2.sharepointonline.com/files/fabric/assets/fonts/leelawadeeui-thai/leelawadeeui-bold.woff -O ./leelawadeeui-thai-bold.woff
